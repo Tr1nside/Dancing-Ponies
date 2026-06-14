9fe6f7a3758088bc05bd5a9478698f003f4feb0e
tr1nside <petya.08.tomsk@gmail.com>
2026-06-14 17:33:23 +0700

    temp data

--- frontend/src/pages/WishlistsPage.tsx
+++ frontend/src/pages/WishlistsPage.tsx
@@ -14,8 +14,14 @@ export default function WishlistsPage() {
     return (
         <div>
             <h1>Мои списки</h1>
+            <div className="new-wishlist-dib">
+                <input type="text" name="" id="emoji-input" />
+                <input type="text" name="" id="title-input" />
+                <input type="button" value="Создать+" />
+
+            </div>
             {wishlists.map((w) => (
-                <div key={w.id} onClick={() => navigate(`/wishlists/${w.id}`)}>
+                <div className="wishlist-div" key={w.id} onClick={() => navigate(`/wishlists/${w.id}`)}>
                     {w.emoji} {w.title}
                 </div>
             ))}
