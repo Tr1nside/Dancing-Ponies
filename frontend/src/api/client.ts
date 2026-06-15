39fd210fb80f61b2f289d8d140f890512fd06701
tr1nside <petya.08.tomsk@gmail.com>
2026-06-16 00:11:29 +0700

    try fix

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -5,9 +5,19 @@ const initDataRaw =
 
 const client = axios.create({
 	baseURL: import.meta.env.VITE_API_URL,
-	headers: {
-		"x-init-data": initDataRaw,
-	},
+	// headers: {
+	// 	"x-init-data": initDataRaw,
+	// },
+});
+
+client.interceptors.request.use((config) => {
+	const initData =
+		window.Telegram?.WebApp?.initData ||
+		import.meta.env.VITE_DEV_INIT_DATA ||
+		"";
+	console.log("Sending initData:", initData.substring(0, 30));
+	config.headers["x-init-data"] = initData;
+	return config;
 });
 
 export default client;
