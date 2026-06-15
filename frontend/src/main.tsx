09941cd8460f30eda4c3a622588e5ba6442aaeb9
tr1nside <petya.08.tomsk@gmail.com>
2026-06-16 00:32:01 +0700

    d

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,11 @@
-if (import.meta.env.DEV) {
-	const eruda = await import("eruda");
-	eruda.default.init();
-}
-
 import { init } from "@telegram-apps/sdk";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import eruda from "eruda";
 import App from "./App.tsx";
 
+eruda.init();
 init();
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
