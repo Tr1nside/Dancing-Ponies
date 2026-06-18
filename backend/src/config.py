66e3a9884b1c0f76ee9a7b930e86f09437bd1ad1
tr1nside <petya.08.tomsk@gmail.com>
2026-06-19 02:54:54 +0700

    черевички

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,11 +1,6 @@
-import os
-from pathlib import Path
 from dotenv import dotenv_values
+from pathlib import Path
 
-config_path = Path(__file__).parent.parent.parent / ".env"
 
-config = {
-    **(dotenv_values(config_path) if config_path.exists() else {}),
-    **os.environ,
-}
-print(config)
+config_path = Path(__file__).parent.parent / ".env"
+config = dotenv_values(config_path)
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,21 +1,14 @@
-import logging
 from fastapi import FastAPI
 from src.routers import invites, wishes, wishlists
 from src.database import engine, Base
-from src.config import config
 from fastapi.middleware.cors import CORSMiddleware
 
-logger = logging.getLogger(__name__)
-
-logger.info("\n\n\n")
-logger.info(config)
-logger.info("\n\n\n")
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # потом заменишь на конкретные домены
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -23,8 +16,6 @@ Base.metadata.create_all(bind=engine)
 app.include_router(invites.router)
 app.include_router(wishes.router)
 app.include_router(wishlists.router)
-
-print(config)
 
 
 @app.get("/")
