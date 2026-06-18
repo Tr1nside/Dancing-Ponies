13e05296bdaefc58c1cd99682bb9226232d27fa5
tr1nside <petya.08.tomsk@gmail.com>
2026-06-19 01:02:23 +0700

    add docker

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,6 +1,10 @@
-from dotenv import dotenv_values
+import os
 from pathlib import Path
-
+from dotenv import dotenv_values
 
 config_path = Path(__file__).parent.parent.parent / ".env"
-config = dotenv_values(config_path)
+
+config = {
+    **(dotenv_values(config_path) if config_path.exists() else {}),
+    **os.environ,
+}
