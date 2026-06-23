d0815a6ecf530cb1532c94b9181a42c5135d294f
tr1nside <petya.08.tomsk@gmail.com>
2026-06-23 16:18:14 +0700

    feat: update security

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,6 @@
 from collections import defaultdict, deque
-import time
 
 from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
